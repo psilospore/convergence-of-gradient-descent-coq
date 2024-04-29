945265613da17af9dee2a2f157b2a6d8eb90ee24
psilospore <syedjafri99@gmail.com>
2024-04-28 20:37:34 -0400

    Up t 6.6 D -> 6.5

--- Main.v
+++ Main.v
@@ -102,8 +102,7 @@ f (y) ≤ f (x) + ∇f (x)T (y − x) + 1
 2 L‖y − x‖2
 2
 *)
-Lemma lipschitz_implies_inequality : forall {n: nat} (x y : Vec n) (f: CostFunction n),
-  exists L : R,
+Lemma lipschitz_implies_inequality : forall {n: nat} (L: R) (x y : Vec n) (f: CostFunction n),
   L > 0 ->
   lipschitz_gradient L (grad f) -> 
   f y <= f x + transpose_mult ((grad f) x) (vector_subtract y x) + (1/2) * L * (L2norm (vector_subtract y x) ^ 2).
@@ -135,8 +134,10 @@ f (x+) ≤ f (x) + ∇f (x)T (x+ − x) + 1/2 * L‖x+ − x‖_2^2
 = f (x) − 1/2 t‖∇f (x)‖_2^2
 *)
 Lemma f_x_plus_less_than : forall {n : nat} (x : Vec n) (f : CostFunction n) (L t : R),
-  (* x+ = x − t∇f (x) *)
   let x_plus := vector_subtract x (scalar_mult t (grad f x)) in
+  f x_plus <= f x + transpose_mult (grad f x) (vector_subtract x_plus x) + 1 / 2 * L * L2norm (vector_subtract x_plus x) ^ 2
+                                                                                                                            ->
+  (* x+ = x − t∇f (x) *)
   f x_plus <= f x - 1/2 * t * ((L2norm (grad f x)) ^ 2).
 Admitted.
 
@@ -290,6 +291,16 @@ Lemma L6_6_C_implies_6_6_A: forall {n: nat} {k: nat} (t: R) (x_0: Vec n) (x_star
 Proof.
 Admitted.
 
+(* Yeonhee and I were making another attempt with the forward strategy *)
+Lemma L6_7_A_implies_6_7_B: forall {n: nat} {k: nat} (t: R) (x_0: Vec n) (x_star: Vec n) (X: X_Points k n) (f : CostFunction n) (x_k : Vec n),
+    (* 6.7 A *)
+  let sums_of_iterations := fold_left (fun (acc :R) (x_i : Vec n) => acc + (f x_i) - (f x_star)) 0 X in
+  f x_k - f x_star <=
+    fold_left (fun (acc : R) (x_i : Vec n) => acc + f x_i - f x_star) 0 X / INR k ->
+  (* 6.7 B *)
+  f x_k - f x_star <= (L2norm (vector_subtract x_0 x_star)) ^ 2 / (2 * t).
+Proof.
+Admitted.
 
 (**
 Theorem 6.1 Suppose the function f : Rn → R is convex and differentiable, and that its gradient is
@@ -299,16 +310,80 @@ which satisfies
           f (x(k)) − f (x∗) ≤ ‖x(0) − x∗‖2
 where f (x∗) is the optimal value. Intuitively, this means that gradient descent is guaranteed to converge
 and that it converges with rate O(1/k).
-*)
-Theorem convergence : forall {n: nat} {k: nat} (t: R) (x_0: Vec n) (x_star: Vec n) (x_k: Vec n) (X: X_Points k n) (f: CostFunction n) (L : R),
+ *)
+
+Theorem L6_2_implies_6_3: forall {n: nat} {k: nat} (t: R) (X: X_Points k n) (f: CostFunction n) (L: R) (i: nat) (Hi: Nat.lt i k) (HSi: Nat.lt (S i) k),
+    let xi := nth X (Fin.of_nat_lt Hi) in
+    let xi_plus_one := nth X (Fin.of_nat_lt HSi) in
+  (* 6.2A *)
+  f xi_plus_one <= f xi + transpose_mult ((grad f) xi) (vector_subtract xi_plus_one xi) + (1/2) * L* (L2norm (vector_subtract xi_plus_one xi)^2)
+  -> (* 6.3 *)
+    f xi_plus_one <= f xi - (1/2 * t) * t * L2norm((grad f) xi)^2.
+Proof.
+Admitted.
+
+Theorem L6_3_implies_6_4_A: forall {n: nat} {k: nat} (t: R) (X: X_Points k n) (x_star: Vec n) (f: CostFunction n) (L: R) (i: nat) (Hi: Nat.lt i k) (HSi: Nat.lt (S i) k),
+    let xi := nth X (Fin.of_nat_lt Hi) in
+    let xi_plus_one := nth X (Fin.of_nat_lt HSi) in
+  (* 6.3 *)
+    f xi_plus_one <= f xi - (1/2 * t) * t * L2norm((grad f) xi)^2 ->
+    (* 6.4 A *)
+    f xi_plus_one <= f x_star + transpose_mult((grad f) xi) (vector_subtract xi x_star) - (t / 2) * L2norm((grad f) xi)^2.
+Proof.
+Admitted.
+
+Theorem L6_4_A_implies_6_4_D: forall {n: nat} {k: nat} (t: R) (X: X_Points k n) (x_star: Vec n) (f: CostFunction n) (L: R) (i: nat) (Hi: Nat.lt i k) (HSi: Nat.lt (S i) k),
+    let xi := nth X (Fin.of_nat_lt Hi) in
+    let xi_plus_one := nth X (Fin.of_nat_lt HSi) in
+  let distance_xi_plus_one_x_star := f xi_plus_one - f x_star in
+    (* 6.4 A *)
+    f xi_plus_one <= f x_star + transpose_mult((grad f) xi) (vector_subtract xi x_star) - (t / 2) * L2norm((grad f) xi)^2 ->
+    (* 6.4 D *)
+
+  (*6.4D *)
+  distance_xi_plus_one_x_star <= 1 / (2*t) * (L2norm (vector_subtract xi x_star)^2 - L2norm(vector_subtract (vector_subtract xi (scalar_mult t ((grad f) xi))) x_star)^2)
+.
+Proof.
+Admitted.
+
+(* 6.4D -> 6.5 *)
+Lemma L6_4_D_implies_6_5: forall {n: nat} {k: nat} (t: R) (X: X_Points k n) (x_star : Vec n) (f: CostFunction n) (i: nat) (Hi: Nat.lt i k) (HSi: Nat.lt (S i) k) ,
+  let xi := nth X (Fin.of_nat_lt Hi) in
+  let xi_plus_one := nth X (Fin.of_nat_lt HSi) in (* why did we define x+ like this*)
+  let distance_xi_plus_one_x_star := f xi_plus_one - f x_star in
+  (*6.4D *)
+  distance_xi_plus_one_x_star <= 1 / (2*t) * (L2norm (vector_subtract xi x_star)^2 - L2norm(vector_subtract (vector_subtract xi (scalar_mult t ((grad f) xi))) x_star)^2)
+  ->
+  (*6.5*)
+  distance_xi_plus_one_x_star <= 1 / (2 * t) * (L2norm (vector_subtract xi x_star)^2 - L2norm(vector_subtract xi_plus_one x_star)^2).
+Admitted.
+
+Theorem convergence : forall {n: nat} {k: nat} (t: R) (x_0: Vec n) (x_star: Vec n) (x_k: Vec n) (X: X_Points k n) (f: CostFunction n) (L : R) (i: nat) (Hi: Nat.lt i k) (HSi: Nat.lt (S i) k) (Hi_minus_one: Nat.lt (i-1) k),
+
   L > 0 ->
   lipschitz L f ->
   convex f ->
   differentiable f ->
   optimal x_star f ->
+  let xi_minus_one := nth X (Fin.of_nat_lt Hi_minus_one) in
+  let xi := nth X (Fin.of_nat_lt Hi) in
+  let xi_plus_one := nth X (Fin.of_nat_lt HSi) in
   f x_k - f x_star <= (L2norm (vector_subtract x_0 x_star)) ^ 2 / (2 * t * (INR k)).
 Proof.
   intros.
+  pose proof (@lipschitz_implies_inequality n L xi xi_plus_one) as MainH.
+  apply (@L6_2_implies_6_3 n k t X f) in MainH.
+  apply (@L6_3_implies_6_4_A n k t X x_star f) in MainH.
+  apply (@L6_4_A_implies_6_4_D n k t X x_star f) in MainH.
+  apply (@L6_4_D_implies_6_5 n k t X x_star f) in MainH.
+
+  rewrite (f_x_plus_less_than) in MainH.
+
+
+    (forall (i: nat) (Hi: Nat.lt i k) (HSi: Nat.lt (S i) k) (Hi_minus_one: Nat.lt (i-1) k),
+
+
+  apply (@lipschitz_implies_inequality n L (nth X (Fin.of_nat_lt _)) (nth X (Fin.of_nat_lt _)) f) in H.
   (*Old strategy *)
   rewrite (Rlt_le_trans_middle _ _ _ (@r1_lt_r2 n k t x_0 x_k x_star X f) (@r2_lt_r3 n k t x_0 x_k x_star X f)).
   apply (L6_7_B_implies_6_7A t x_0).
@@ -318,6 +393,10 @@ Proof.
       acc + f x_i - f x_star) 0 X / INR k *)
   apply (L6_6_C_implies_6_6_A t x_0 x_star X f).
 
+(**
+We decided thanks to Steven that we can just introduce hypothesis moving forward.
+e.g. we have 6.2 or right before 6.1
+*)
 
 End Main.
 
